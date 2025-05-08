70618e0d4a911b9923cc8db2d7aa55bd1f015a47
Mark <mark-sw@yandex.ru>
2025-05-08 11:42:48 +0300

    Fix the line lightning

--- assets/scripts/niki_entry.js
+++ assets/scripts/niki_entry.js
@@ -1,19 +1,21 @@
-console.log("ura");
-document.querySelectorAll(".grid-el").forEach(cell => {
-	cell.addEventListener("mouseenter", () => {
-		let allCells = document.querySelectorAll(".grid-el");
-		let index = Array.from(allCells).indexOf(cell);
-		let rowStart = Math.floor(index/5)*5;
-		for (let i= rowStart; i<rowStart + 5; i++) {
-			allCells[i].style.backgroundColor = "blanchedalmond";
-		}
+function addRows() {
+	document.querySelectorAll(".grid-el").forEach(cell => {
+		cell.addEventListener("mouseenter", () => {
+			let allCells = document.querySelectorAll(".grid-el");
+			let index = Array.from(allCells).indexOf(cell);
+			// allCells[index].style.backgroundColor = "black";
+			let rowStart = Math.floor(index/5)*5;	// здесь мы получаем номер строки
+			for (let i= rowStart; i<rowStart + 5; i++) {
+				allCells[i].style.backgroundColor = "blanchedalmond";
+			}
+		});
+		cell.addEventListener("mouseleave", () => {
+			let allCells = document.querySelectorAll(".grid-el");
+			let index = Array.from(allCells).indexOf(cell);
+			let rowStart = Math.floor(index/5)*5;
+			for (let i= rowStart; i<rowStart + 5; i++) {
+				allCells[i].style.backgroundColor = "";
+			}
+		});
 	});
-	cell.addEventListener("mouseleave", () => {
-		let allCells = document.querySelectorAll(".grid-el");
-		let index = Array.from(allCells).indexOf(cell);
-		let rowStart = Math.floor(index/5)*5;
-		for (let i= rowStart; i<rowStart + 5; i++) {
-			allCells[i].style.backgroundColor = "";
-		}
-	});
-});
+}
